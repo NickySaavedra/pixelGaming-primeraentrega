164f02e67ec170cd5bd48c4655ceb4fb3039c34b
NickySaavedra <nicky.zuky@gmail.com>
2023-04-21 15:07:22 -0300

    primera pre entrega

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>PixelGaming</h3>
+            <div>
+                <button>Monitores</button>
+                <button>Placas madres</button>
+                <button>Teclados</button>
+                <button>Procesadores</button>
+                <button>Tarjetas gráficas</button>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar
